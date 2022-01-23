6eea9ece49c56587e92dc8feccdfc0102127845a
Tamminana.sumith kumar <96720244+Sumithkumar1@users.noreply.github.com>
2022-01-23 15:22:04 +0530

    Add files via upload

---
+++ 1` to n printing.cpp
@@ -0,0 +1,15 @@
+//pgrom to printno from 1 to n using while loop//
+#include<stdio.h>
+int main()
+{
+	int i=1,n;
+	printf("ene]ter a number/n");
+	scanf("%d",&n);
+	while(i<=n){
+		printf("%d/n",i);
+		i++;
+		
+	}
+		
+	
+}
